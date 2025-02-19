25db7c6219b3c13b98c6b28ef8c64a932cfc2aa8
Александр Быков <150475371+Jimaet@users.noreply.github.com>
2025-02-19 15:56:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.querySelector(".carousel-container");
+    function enableCarouselScrolling(carousel) {
+        let isDown = false;
+        let startX;
+        let scrollLeft;
 
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+        carousel.addEventListener("mousedown", (e) => {
+            isDown = true;
+            startX = e.pageX - carousel.offsetLeft;
+            scrollLeft = carousel.scrollLeft;
+        });
 
-    carousel.addEventListener("mousedown", (e) => {
-        isDown = true;
-        startX = e.pageX - carousel.offsetLeft;
-        scrollLeft = carousel.scrollLeft;
-    });
+        carousel.addEventListener("mouseleave", () => {
+            isDown = false;
+        });
 
-    carousel.addEventListener("mouseleave", () => {
-        isDown = false;
-    });
+        carousel.addEventListener("mouseup", () => {
+            isDown = false;
+        });
 
-    carousel.addEventListener("mouseup", () => {
-        isDown = false;
-    });
+        carousel.addEventListener("mousemove", (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - carousel.offsetLeft;
+            const walk = (x - startX) * 2;
+            carousel.scrollLeft = scrollLeft - walk;
+        });
 
-    carousel.addEventListener("mousemove", (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - carousel.offsetLeft;
-        const walk = (x - startX) * 2;
-        carousel.scrollLeft = scrollLeft - walk;
-    });
+        // Поддержка мобильного свайпа
+        let touchStartX = 0;
+        let touchEndX = 0;
 
-    // Поддержка мобильного свайпа
-    let touchStartX = 0;
-    let touchEndX = 0;
+        carousel.addEventListener("touchstart", (e) => {
+            touchStartX = e.touches[0].clientX;
+        });
 
-    carousel.addEventListener("touchstart", (e) => {
-        touchStartX = e.touches[0].clientX;
-    });
+        carousel.addEventListener("touchmove", (e) => {
+            touchEndX = e.touches[0].clientX;
+            let moveX = touchStartX - touchEndX;
+            carousel.scrollLeft += moveX * 1.5;
+        });
 
-    carousel.addEventListener("touchmove", (e) => {
-        touchEndX = e.touches[0].clientX;
-        let moveX = touchStartX - touchEndX;
-        carousel.scrollLeft += moveX * 1.5;
-    });
+        carousel.addEventListener("touchend", () => {
+            touchStartX = 0;
+            touchEndX = 0;
+        });
+    }
 
-    carousel.addEventListener("touchend", () => {
-        touchStartX = 0;
-        touchEndX = 0;
-    });
+    const carousels = document.querySelectorAll(".carousel-container");
+    carousels.forEach(enableCarouselScrolling);
 });
