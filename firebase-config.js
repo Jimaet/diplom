1fbf66995d01c7fd88f84036ab32ee55decbec12
Александр Быков <150475371+Jimaet@users.noreply.github.com>
2025-03-02 15:36:38 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqIDTQrS14wTLsh_jFkD0GZAmEEWW8TDk",
+    authDomain: "cooker-62216.firebaseapp.com",
+    projectId: "cooker-62216",
+    storageBucket: "cooker-62216.firebasestorage.app",
+    messagingSenderId: "994568659489",
+    appId: "1:994568659489:web:18c15bc15fa5b723a03960"
+};
+
+// Инициализация Firebase
+export const app = initializeApp(firebaseConfig);
