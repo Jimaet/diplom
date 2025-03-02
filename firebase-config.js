9ba9cf0355dbdcd9dd64c03354a1f8b44f649d06
Александр Быков <150475371+Jimaet@users.noreply.github.com>
2025-03-02 15:51:34 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDqIDTQrS14wTLsh_jFkD0GZAmEEWW8TDk",
     authDomain: "cooker-62216.firebaseapp.com",
     projectId: "cooker-62216",
-    storageBucket: "cooker-62216.appspot.com", // Исправлено
+    storageBucket: "cooker-62216.appspot.com",
     messagingSenderId: "994568659489",
     appId: "1:994568659489:web:18c15bc15fa5b723a03960"
 };
 
+// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-console.log("Firebase подключен:", app);
-console.log("Firestore:", db);
-
-export { db };
+export const db = getFirestore(app);
