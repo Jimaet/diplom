952bbf799c01be4356370270465fd02f2afe7635
Александр Быков <150475371+Jimaet@users.noreply.github.com>
2025-03-02 15:31:42 +0300

    Update addRecipe.js

--- addRecipe.js
+++ addRecipe.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDqIDTQrS14wTLsh_jFkD0GZAmEEWW8TDk",
     authDomain: "cooker-62216.firebaseapp.com",
     projectId: "cooker-62216",
-    storageBucket: "cooker-62216.firebasestorage.app",
+    storageBucket: "cooker-62216.appspot.com",
     messagingSenderId: "994568659489",
     appId: "1:994568659489:web:18c15bc15fa5b723a03960"
 };
