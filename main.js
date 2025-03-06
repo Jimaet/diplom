f79f43f7b2b7bf61f768cfadcce3203905790bbf
Александр Быков <150475371+Jimaet@users.noreply.github.com>
2025-03-06 14:07:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// 🔹 Импорт Firebase 🔹
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, doc, collection, getDocs, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
